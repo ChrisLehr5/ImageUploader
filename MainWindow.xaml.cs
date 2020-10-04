7610efd44ab856de41baafa46b5fbfa7e86c51c4
chrislargent <christine.com>
2020-10-04 15:11:42 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Media;
+using GSF.IO;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            if (op.ShowDialog() == true)
+            {
+                imgPhoto.Source = new BitmapImage(new Uri(op.FileName));
+            }
+        }
+
+        //private string choosenFileName;
+        public void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = txtText.Text + ".png";
+            SaveControlImage(grdText, filename);
+            MessageBox.Show("Done");
+
+        }
+        // Save a control's image.
+        // Save a control's image.
+        private void SaveControlImage(FrameworkElement control,
+            string filename)
+        {
+            // Get the size of the Visual and its descendants.
+            Rect rect = VisualTreeHelper.GetDescendantBounds(control);
+
+            // Make a DrawingVisual to make a screen
+            // representation of the control.
+            DrawingVisual dv = new DrawingVisual();
+
+            // Fill a rectangle the same size as the control
+            // with a brush containing images of the control.
+            using (DrawingContext ctx = dv.RenderOpen())
+            {
+                VisualBrush brush = new VisualBrush(control);
+                ctx.DrawRectangle(brush, null, new Rect(rect.Size));
+            }
+
+            // Make a bitmap and draw on it.
+            int width = (int)control.ActualWidth;
+            int height = (int)control.ActualHeight;
+            RenderTargetBitmap rtb = new RenderTargetBitmap(
+                width, height, 96, 96, PixelFormats.Pbgra32);
+            //changing to 50,50 forces smaller
+            rtb.Render(dv);
+
+            // Make a PNG encoder.
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+
+            // Save the file.
+            //using (FileStream fs = new FileStream(filename,
+            //    FileMode.Create, FileAccess.Write, FileShare.None))
+            //{
+            //   encoder.Save(fs);
+            //  }
+
+            // Save the file.           
+            FileStream fileObj = new FileStream(@"C:\\Users\\Khyr\\Source\\Repos\\WpfApp1\\Images\\" + filename, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+
+            {
+                encoder.Save(fileObj);
+             }
+
+
+
+
+        }
+    }
+}
+             
+    
